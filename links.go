c24ac5ae9bf5f6ab1621cca1d65158f140b9fb25
Nikolay Tupitsyn <nikolaideveloper@gmail.com>
2023-09-09 18:54:33 +0300

    Розiбрали передачу параметрiв у функцiю за посиланням

---
+++ links.go
@@ -0,0 +1,27 @@
+package main
+
+
+import (
+	"fmt"
+
+)
+
+func setDataByLinks(variable *int) {
+	fmt.Println("number in function = ", *variable)
+	*variable = 30
+	fmt.Println("number in function = ", *variable)
+
+}
+
+
+func main(){
+	number := 0
+	fmt.Println("number = ", number)
+
+	setDataByLinks(&number)
+
+	fmt.Println("number = ", number)
+//	fmt.Println(string(number))
+}
+
+
